1d06d762df83fa0fff8140846bf747222150f637
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-04-16 19:55:04 +0530

    Initial msg

---
+++ Pattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            for (x = 8; x >= 1; x--)
+            {
+                for (y = 1; y <= x; y++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
+
---
+++ ReverseString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversestring = "";
+            int length;
+
+            length = stringInput.Length - 1;
+
+            while (length >= 0)
+            {
+                reversestring = reversestring + stringInput[length];
+                length--;
+            }
+            Console.WriteLine(reversestring);
+
+            Console.ReadLine();
+
+        }
+        public static void Main(string[] args)
+        {
+            ReverseString("nutan");
+        }
+    }
+    }
+
